20b6d2088506e5fd2bade61cf8c16391412f47d3
issei <issei.morimoto@gmail.com>
2018-07-12 18:29:08 +0200

    ex01

---
+++ j06/ex01/ft_print_program_name.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imorimot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/12 17:54:08 by imorimot          #+#    #+#             */
+/*   Updated: 2018/07/12 18:26:34 by imorimot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
